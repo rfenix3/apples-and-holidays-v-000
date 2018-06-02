c6a1925d891eecce0bb9b25b9ed43f7c0703a34f
Richard Fenix <richard.fenix@gmail.com>
2018-06-02 00:44:27 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,7 +32,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, holiday|
     if season == :winter
         holiday.each do |items|
-          items.push << supply
+          items.push(supply)
         end
     end
   end
